a777921311f45e48d40fc34863884d3a73dc84e9
Vinicius.hans <hanelvinicius@gmail.com>
2023-05-16 16:42:31 -0300

    adcionado testes automatizados de querys

--- src/main/java/br/com/alura/bytebank/test/TestaQuery.java
+++ src/main/java/br/com/alura/bytebank/test/TestaQuery.java
@@ -1,22 +1,50 @@
 package br.com.alura.bytebank.test;
 
 import br.com.alura.bytebank.ConnectionFactory;
+import br.com.alura.bytebank.domain.conta.Conta;
 import br.com.alura.bytebank.domain.conta.ContaDAO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.Set;
 
 
 public class TestaQuery {
 
-    public static void main(String[] args) {
-        testaPegaConta();
+       ConnectionFactory conn;
+       ContaDAO contaDao;
+
+    @BeforeEach
+    public void init() {
+        this.conn = new ConnectionFactory();
+        this.contaDao = new ContaDAO(conn.recuperaConexao());
     }
 
-    public static void testaPegaConta(){
-        ConnectionFactory conn = new ConnectionFactory();
+    @Test
+    public void testaPegaConta(){
 
-        ContaDAO contaDao = new ContaDAO(conn.recuperaConexao());
-        System.out.println(contaDao.pegaConta(90909));
-
+        Assertions.assertEquals(this.contaDao.pegaConta(90909).getClass(), Conta.class);
+        Assertions.assertThrows(RuntimeException.class,() -> {
+            this.contaDao.listar();
+        });
     }
+    @Test
+    public void testaListagem(){
+        Set<Conta> contas = this.contaDao.listar();
+        // o banco nunca estara vazio pois contem um valor de teste portanto se vier vazio é porque houve alguma falha
+        Assertions.assertFalse(contas.isEmpty());
+        Assertions.assertThrows(RuntimeException.class,() -> {
+            this.contaDao.listar();
+        });
+    }
+
+
+
+
+
 
 
 }
